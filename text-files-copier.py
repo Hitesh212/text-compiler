111de5c49d6f8b6f9c26bae646500e7e3232229f
Zhiwen Huang <zhiwen555@gmail.com>
2016-04-18 03:41:56 -0400

    added ability to filter keywords, 4x sub directory, and improvements

--- text-files-copier.py
+++ text-files-copier.py
@@ -1,120 +1,224 @@
 #! /usr/bin/python3
 
-# The purpose of this Python script is to go through a folder, copy all the text within
-# a designated file type, and write them into a designated text file. The script labels
+# The purpose of this Python script is to transverse a directory, copy all the text within
+# designated file types, and write them into a designated file. The script labels
 # and formats the copied files based on their folder locations and file names.
-
-# Can read files within sub-sub-directories.
+# Can read files within sub x4 directories
 
 import os
-import time
 import sys
+import re
 
+## Globals
+# adjust these variables
 
-# Adjust these variables
+# the file you are writing or appending to
+write_file_name = "/home/zhiwen/Desktop/copier-test.txt"
+mode = "w"
+# check to see if there currently is a file of same name
+if os.path.isfile(write_file_name):
+    replace = input("{} is already a file, to replace file, input 'y': ".format(write_file_name))
+    if replace != 'y':
+        print("Not replacing")
+        sys.exit()
+write_file = open(write_file_name, mode)
 
-# The file you are writing or appending to
-write_file = open("/home/zhiwen/Desktop/notes.txt", "a+")
+# file extensions that will be read
+extension = (".py",".html",".js")
 
-# Extension of file that will be read
-extension = ".py"
+# number of sub directory levels to transverse [0-4]
+level = 4
 
-# Path to initial directory
-folder = "Python 3 Essential Training"
-folder_directory_path = "/media/removable/SD Card/Lynda/"
+# path to initial directory
+folder = ""
+folder_directory_path = "/media/removable/SD Card/Lynda/" # needs a / at end
 initial_directory_path = folder_directory_path + folder
 
+# keywords to avoid reading or opening. Case sensitive. List None if not using.
+exclude_file = None
+exclude_folder = None
+
+# initial for amount of files copied
+count = 0
 
 
-# copy and write entire file
-def copy_file(file):
-    print("### ", file, " ###", file = write_file)
+
+## Functions
+        
+# tests to make sure the globals are valid
+def tests():
+    # initial directory test and set initial directory
+    try:
+        os.chdir(initial_directory_path)
+    except:
+        print("Invalid initial directory path")
+        sys.exit()
+
+    # exclude file keyword test
+    global patternfile
+    if exclude_file != None:
+        try:
+            patternfile = re.compile(exclude_file)
+        except:
+            print("Invalid file keyword")
+            sys.exit()
+            
+    # exclude folder keyword test
+    global patternfolder
+    if exclude_folder != None:
+        try:
+            patternfolder = re.compile(exclude_folder)
+        except:
+            print("Invalid folder keyword")
+            sys.exit()
+
+
+# exclude directories from directory list based on keywords
+def exclude(directory_list):
+    excluded_list = []
+
+    if (exclude_folder != None) or (exclude_file != None):
+
+        # patternfolder and patternfile are initalized in test()
+        for directory in directory_list:
+            if os.path.isdir(directory) and exclude_folder != None:
+                if not re.search(patternfolder,directory):
+                    excluded_list.append(directory)
+                    
+            elif os.path.isfile(directory) and exclude_file != None:
+                if not re.search(patternfile, directory):
+                    excluded_list.append(directory)
+                    
+            else:
+                excluded_list.append(directory)
+    else:
+        excluded_list = directory_list
+        
+    return excluded_list
+
+
+# sort a directory list in alphabetical order with folders first then files
+def sort_dir_list(directory_list): 
+    sorted_list = []
+    
+    directory_list.sort()
+
+    for directory in directory_list:
+        if os.path.isdir(directory):
+            sorted_list.append(directory)
+            
+    for directory in directory_list:
+        if os.path.isfile(directory):
+            sorted_list.append(directory)
+            
+    return sorted_list
+
+
+# create directory create and manipulate its directory list
+def setdirectory(directory):
+    # set directory
+    os.chdir(directory)
+
+    # get directory list
+    directory_list = os.listdir()
+
+    # exclude directory list
+    directory_list = exclude(directory_list)
+          
+    # sort directory list
+    directory_list = sort_dir_list(directory_list)
+   
+    # directory name format
+    print("######## ",directory," ########", file = write_file)
+    print(' ', file = write_file)
+
+    print("Going into directory \"{}\"".format(directory))
+    print()
+    return directory_list
+
+
+# read and write file
+def copyfile(file):
+    # file header format
+    print("##### ", file, " #####", file = write_file)
     print(' ', file = write_file)
     read_file = open(file, 'r')
     
     # iterate through every line in file and write
     for line in read_file:
         print(line, file = write_file, end = '')
-        
-    print("#==========#", file = write_file)
+
+    # file ending format    
+    print('_______________________________________', file = write_file)
+    print(' ', file = write_file)
     print(' ', file = write_file)
     
-    print('Copied {} '.format(file))
+    print('Copying "{}" '.format(file))
     print(' ')
-
-
-# sort a directory list in alphabetical order with folders first then files
-def sort_dir_list(directory_list):
-    directory_list.sort()
-    sorted_list = []
     
-    for list in directory_list:
-        if os.path.isdir(list):
-            sorted_list.append(list)
-            
-    for list in directory_list:
-        if os.path.isfile(list):
-            sorted_list.append(list)
-            
-    return sorted_list
+    global count
+    count += 1
 
-# create subdirectory and return its directory list
-def subdirectory(directory):
-    # set directory
-    os.chdir(directory)
     
-    # get list and sort
-    sub_directory_list = os.listdir()
-    sub_directory_list = sort_dir_list(sub_directory_list)
-    
-    # write the directory name to file
-    print("###### ",directory," ######", file = write_file)
-    print(' ', file = write_file)
-    return sub_directory_list
-
-
-
+## main
 def main():
+    tests()
+                      
     # write the folder name into file
-    print("######### ", folder, " #########", file = write_file)
+    print("############ ", folder, " ############", file = write_file)
     print(" ", file = write_file)
-    
-    # set working directory
-    try:
-        os.chdir(initial_directory_path)
-    except:
-        print("Invalid initial directory path")
-        time.sleep(4)
-        sys.exit()
-        
+      
     # create directory list and sort it
     directory_list = os.listdir(initial_directory_path)
+    directory_list = exclude(directory_list)
     directory_list = sort_dir_list(directory_list)
-
-    # iterate through all folders and files down to sub sub directories
+    
+    # iterate through all folders and files down to sub x4 directories
     for directory in directory_list:
         os.chdir(initial_directory_path)
-        if os.path.isdir(directory):
-            sub_directory_list = subdirectory(directory)
+        if os.path.isdir(directory) and (level >= 1):
+            sub_directory_list = setdirectory(directory)
             sub_directory_path = os.getcwd()
             for sub_directory in sub_directory_list:
                 os.chdir(sub_directory_path)
-                if os.path.isdir(sub_directory):
-                    sub_sub_directory_list = subdirectory(sub_directory)
-                    for sub_sub_directory in sub_sub_directory_list:
-                        if sub_sub_directory.endswith(extension):
-                            copy_file(sub_sub_directory)
+                if os.path.isdir(sub_directory) and (level >= 2):
+                    sub2_directory_list = setdirectory(sub_directory)
+                    sub2_directory_path = os.getcwd()
+                    for sub2_directory in sub2_directory_list:
+                        os.chdir(sub2_directory_path)
+                        if os.path.isdir(sub2_directory) and (level >= 3):
+                            sub3_directory_list = setdirectory(sub2_directory)
+                            sub3_directory_path = os.getcwd()
+                            for sub3_directory in sub3_directory_list:
+                                os.chdir(sub3_directory_path)
+                                if os.path.isdir(sub3_directory) and (level >= 4):
+                                    sub4_directory_list = setdirectory(sub3_directory)
+                                    sub4_directory_path = os.getcwd()
+                                    for sub4_directory in sub4_directory_list:
+                                        #os.chdir(sub4_directory_path)
+                                        if sub4_directory.endswith(extension):
+                                            copyfile(sub4_directory)
+
+                                elif sub3_directory.endswith(extension):
+                                    copyfile(sub3_directory)
+                                else: pass
+                                               
+                        elif sub2_directory.endswith(extension):
+                            copyfile(sub2_directory)
+                        else: pass
                             
                 elif sub_directory.endswith(extension):
-                    copy_file(sub_directory)
+                    copyfile(sub_directory)
                 else: pass
                 
         elif directory.endswith(extension):
-            copy_file(directory)
+            copyfile(directory)
         else: pass
 
     # close file
     write_file.close()
+    print("Done! Copied {} files into {}".format(count, write_file_name))
+    
 
 if __name__ == "__main__":
     main()
